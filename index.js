519ec1e1dd6bfa0e20114b6b7fd38a72c49c8fb5
Kalyan Vedala <svedala23@gmail.com>
2025-11-11 23:51:48 -0800

    Use cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 4000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.post('/messages', async (req, res) => {
