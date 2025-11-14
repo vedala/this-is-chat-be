a09b48643c8abd77a3620941331cef4cf6ed0383
Kalyan Vedala <svedala23@gmail.com>
2025-11-13 22:43:21 -0800

    Remove sqlite-related code

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
 import { MongoClient } from 'mongodb';
-
-const db = await open({
-  filename: 'chatapp.db',
-  driver: sqlite3.Database,
-});
 
 const mongoClient = new MongoClient(
   `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@${process.env.MONGO_BASE_URL}`
@@ -17,13 +10,6 @@ const mongoClient = new MongoClient(
 
 await mongoClient.connect();
 const dbTemp = mongoClient.db("chatdb");
-
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    message TEXT
-  );
-`);
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -36,7 +22,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/messages', async (req, res) => {
-  const rows = await db.all("SELECT * FROM messages");
   const collection = dbTemp.collection("messages");
   const documents = await collection.find({}).toArray();
 console.log(documents);
@@ -46,18 +31,13 @@ console.log(documents);
 app.post('/messages', async (req, res) => {
   const { message } = req.body;
   console.log(`Received message: ${message}`);
-  let result;
-  try {
-    result = await db.run('INSERT INTO messages (message) VALUES (?)', message);
-  } catch(e) {
-    console.log("Error saving message to db");
-    res.status(500).json("Error saving message to db");
-  }
 
   try {
-    const result = await dbTemp.collection("messages").insertOne({
+    const response = await dbTemp.collection("messages").insertOne({
       message
     });
+
+    console.log("insertOne response=", response);
   } catch (e) {
     console.log("Error saving message to dbTemp, e=", e);
   }
