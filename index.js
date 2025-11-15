3247342ac10c4c9a7b207567471d95ca97bee521
Kalyan Vedala <svedala23@gmail.com>
2025-11-14 20:15:50 -0800

    Split index.js into app.js

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import { connectDB, getDB } from './db.js';
+import app from "./app.js";
+import { connectDB } from './db.js';
 
-await connectDB();
-
-const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
-app.use(cors());
-
-app.get('/messages', async (req, res) => {
-  const collection = await getDB().collection("messages");
-  const documents = await collection.find({}).toArray();
-console.log(documents);
-  res.json(documents);
-});
-
-app.post('/messages', async (req, res) => {
-  const { message } = req.body;
-  console.log(`Received message: ${message}`);
-
-  let response;
-  try {
-    response = await getDB().collection("messages").insertOne({
-      message
-    });
-
-    console.log("insertOne response=", response);
-  } catch (e) {
-    console.log("Error saving message to db, e=", e);
-  }
-
-  res.status(200).json(response.insertedId);
-});
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
