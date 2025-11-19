fb80731631dd0df989bf15ebd8a7ee3580aba0e9
Kalyan Vedala <svedala23@gmail.com>
2025-11-18 22:47:52 -0800

    Setup ws

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
 import 'dotenv/config';
 import express from 'express';
+import http from "http";
 import cors from 'cors';
 import { getDB } from './db.js';
+import { WebSocketServer } from 'ws';
 
 const MESSAGES_COLLECTION = process.env.MESSAGES_COLLECTION;
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 
-app.get('/messages', async (req, res) => {
-  const collection = getDB().collection(MESSAGES_COLLECTION);
-  const documents = await collection.find({}).toArray();
 
-  res.json(documents);
-});
 
-app.post('/messages', async (req, res) => {
-  const { message } = req.body;
-  console.log(`Received message: ${message}`);
+console.log("about to setup wss");
+wss.on("connection", async(ws) => {
+  console.log("Client connected");
 
-  let response;
-  try {
-    response = await getDB().collection(MESSAGES_COLLECTION).insertOne({
-      message
+  ws.on("message", async (raw) => {
+    const msg = JSON.parse(raw);
+    console.log("Message event, msg=", msg);
+
+    const saved = await getDB().collection(MESSAGES_COLLECTION).insertOne({
+      message: msg.text
     });
-
-    console.log("insertOne response=", response);
-  } catch (e) {
-    console.log("Error saving message to db, e=", e);
-  }
-
-  res.status(200).json(response.insertedId);
+  });
 });
 
-export default app;
+
+
+// app.get('/messages', async (req, res) => {
+//   const collection = getDB().collection(MESSAGES_COLLECTION);
+//   const documents = await collection.find({}).toArray();
+
+//   res.json(documents);
+// });
+
+// app.post('/messages', async (req, res) => {
+//   const { message } = req.body;
+//   console.log(`Received message: ${message}`);
+
+//   let response;
+//   try {
+//     response = await getDB().collection(MESSAGES_COLLECTION).insertOne({
+//       message
+//     });
+
+//     console.log("insertOne response=", response);
+//   } catch (e) {
+//     console.log("Error saving message to db, e=", e);
+//   }
+
+//   res.status(200).json(response.insertedId);
+// });
+
+
+
+export default server;
