8d35184e504deb9c09b0be6613417cae3ab4d3ab
Kalyan Vedala <svedala23@gmail.com>
2025-12-09 03:59:44 -0800

    Add middleware function to check auth0 token

---
+++ authenticate.js
@@ -0,0 +1,16 @@
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
+
+const checkJwt = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ["RSA256"],
+});
+
+export default checkJwt;
